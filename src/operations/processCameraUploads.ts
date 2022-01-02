95c987ba77554709a7c266a44e8850b66b1cf8b4
Rachel Evans <rachel@rachelevans.org>
2022-01-02 17:38:28 +0100

    Also move png and videos

--- src/operations/processCameraUploads.ts
+++ src/operations/processCameraUploads.ts
@@ -2,12 +2,34 @@ import { DropboxProvider, GlobalOptions, Handler } from "../types";
 import { processOptions } from "../options";
 import lister from "../lister";
 import Mover from "../mover";
+import { files } from "dropbox";
 
 const verb = "process-camera-uploads";
 const CAMERA_UPLOADS = "/Camera Uploads";
 
 const TAIL = "--tail";
 const DRY_RUN = "--dry-run";
+
+const targetForFile = (item: files.FileMetadata): string | undefined => {
+  if (item.path_display === undefined) return undefined;
+  if (item.path_lower === undefined) return undefined;
+
+  const yyyy = item.client_modified.substring(0, 4);
+  const yyyymm = item.client_modified.substring(0, 7);
+  const yyyymmdd = item.client_modified.substring(0, 10);
+  const parts = item.path_display.split("/");
+  const basename = parts[parts.length - 1];
+
+  if (item.path_lower.endsWith(".jpg") || item.path_lower.endsWith(".png")) {
+    return `/pics/camera/sliced/${yyyy}/${yyyymm}/${yyyymmdd}/${basename}`;
+  }
+
+  if (item.path_lower.endsWith(".mov") || item.path_lower.endsWith(".mp4")) {
+    return `/pics/videos/sliced/${yyyy}/${yyyymm}/${yyyymmdd}/${basename}`;
+  }
+
+  return undefined;
+};
 
 const handler: Handler = async (
   dbxp: DropboxProvider,
@@ -29,6 +51,62 @@ const handler: Handler = async (
 
   const mover = new Mover(dbx, globalOptions);
 
+  const tryMove = async (
+    item: files.FileMetadata,
+    wantedPath: string
+  ): Promise<void> => {
+    if (!item.path_display) return;
+
+    const existing = await dbx.filesGetMetadata({ path: wantedPath }).then(
+      (r) => r.result,
+      (err) => {
+        if (
+          err.status === 409 &&
+          err.error?.error_summary.includes("path/not_found")
+        )
+          return undefined;
+
+        console.log(JSON.stringify({ get_existing: err }));
+        throw err;
+      }
+    );
+
+    // console.log(` == ${JSON.stringify(existing)}`);
+
+    if (existing === undefined) {
+      console.log(`  -> ${wantedPath}`);
+
+      if (!dryRun) {
+        return mover.submit({
+          from_path: item.path_display,
+          to_path: wantedPath,
+        });
+      }
+    } else {
+      console.log(`  declining to move because the target exists:`);
+      console.log(`  ${JSON.stringify(existing)}`);
+
+      if (existing[".tag"] === "file") {
+        if (existing.content_hash === item.content_hash) {
+          if (existing.client_modified === item.client_modified) {
+            console.log(`  identical - will remove source`);
+            if (!dryRun) {
+              shutdownWaitsFor(
+                dbx.filesDeleteV2({ path: item.id }).catch(() => undefined)
+              );
+            }
+          } else {
+            console.log(`  mismatching mtimes`);
+          }
+        } else {
+          console.log(`  different content`);
+        }
+      } else {
+        console.log(`  file/dir mismatch`);
+      }
+    }
+  };
+
   let shutdownPromise = Promise.resolve();
   const shutdownWaitsFor = (p: Promise<unknown>): void => {
     shutdownPromise = Promise.all([
@@ -44,72 +122,8 @@ const handler: Handler = async (
       console.log(JSON.stringify(item));
 
       if (item[".tag"] === "file" && item.path_lower && item.path_display) {
-        if (item.path_lower.endsWith(".jpg")) {
-          const yyyy = item.client_modified.substring(0, 4);
-          const yyyymm = item.client_modified.substring(0, 7);
-          const yyyymmdd = item.client_modified.substring(0, 10);
-          const parts = item.path_display.split("/");
-
-          const wantedPath = `/pics/camera/sliced/${yyyy}/${yyyymm}/${yyyymmdd}/${
-            parts[parts.length - 1]
-          }`;
-          // console.log(` -> ${wantedPath}`);
-
-          const existing = await dbx
-            .filesGetMetadata({ path: wantedPath })
-            .then(
-              (r) => r.result,
-              (err) => {
-                if (
-                  err.status === 409 &&
-                  err.error?.error_summary.includes("path/not_found")
-                )
-                  return undefined;
-
-                console.log(JSON.stringify({ get_existing: err }));
-                throw err;
-              }
-            );
-
-          // console.log(` == ${JSON.stringify(existing)}`);
-
-          if (existing === undefined) {
-            console.log(`  -> ${wantedPath}`);
-
-            if (!dryRun) {
-              shutdownWaitsFor(
-                mover.submit({
-                  from_path: item.path_display,
-                  to_path: wantedPath,
-                })
-              );
-            }
-          } else {
-            console.log(`  declining to move because the target exists:`);
-            console.log(`  ${JSON.stringify(existing)}`);
-
-            if (existing[".tag"] === "file") {
-              if (existing.content_hash === item.content_hash) {
-                if (existing.client_modified === item.client_modified) {
-                  console.log(`  identical - will remove source`);
-                  if (!dryRun) {
-                    shutdownWaitsFor(
-                      dbx
-                        .filesDeleteV2({ path: item.id })
-                        .catch(() => undefined)
-                    );
-                  }
-                } else {
-                  console.log(`  mismatching mtimes`);
-                }
-              } else {
-                console.log(`  different content`);
-              }
-            } else {
-              console.log(`  file/dir mismatch`);
-            }
-          }
-        }
+        const wantedPath = targetForFile(item);
+        if (wantedPath) return shutdownWaitsFor(tryMove(item, wantedPath));
       }
 
       return Promise.resolve();
