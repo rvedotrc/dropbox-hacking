94df287a9728f8e37aa1103b75956342193d9fc2
Rachel Evans <rachel@rachelevans.org>
2022-12-27 18:01:10 +0000

    Allow deferred job tag generation

--- src/util/promises/promiseLimiter.ts
+++ src/util/promises/promiseLimiter.ts
@@ -1,16 +1,20 @@
 import { GlobalOptionsSingleton } from "../../globalOptions";
 
+type Tag = string | (() => string);
+
 type Entry<T> = {
   id: number;
   makePromise: () => Promise<T>;
   resolve: (value: T | PromiseLike<T>) => void;
   reject: (reason?: any) => void; // eslint-disable-line @typescript-eslint/no-explicit-any
-  tag: string;
+  tag: Tag;
 };
 
 export type PromiseLimiter<T> = {
-  submit: (makePromise: () => Promise<T>, tag: string) => Promise<T>;
+  submit: (makePromise: () => Promise<T>, tag: Tag) => Promise<T>;
 };
+
+const tagToString = (tag: Tag) => (typeof tag === "string" ? tag : tag());
 
 export const makePromiseLimiter = <T>(
   size: number,
@@ -36,14 +40,18 @@ export const makePromiseLimiter = <T>(
       inFlight.add(entry);
       if (GlobalOptionsSingleton.get()?.debugLimiter)
         console.debug(
-          `${name} start job #${id}/${nextId} ${tag} (in flight: ${describe()})`
+          `${name} start job #${id}/${nextId} ${tagToString(
+            tag
+          )} (in flight: ${describe()})`
         );
       makePromise()
         .finally(() => {
           inFlight.delete(entry);
           if (GlobalOptionsSingleton.get()?.debugLimiter)
             console.debug(
-              `${name} end job #${id}/${nextId} ${tag} (in flight: ${describe()})`
+              `${name} end job #${id}/${nextId} ${tagToString(
+                tag
+              )} (in flight: ${describe()})`
             );
           ++free;
           tryStart();
@@ -54,10 +62,10 @@ export const makePromiseLimiter = <T>(
 
   let nextId = 0;
 
-  const submit = (makePromise: () => Promise<T>, tag: string): Promise<T> => {
+  const submit = (makePromise: () => Promise<T>, tag: Tag): Promise<T> => {
     const id = nextId++;
     if (GlobalOptionsSingleton.get()?.debugLimiter)
-      console.debug(`${name} submit job #${id} ${tag}`);
+      console.debug(`${name} submit job #${id} ${tagToString(tag)}`);
     return new Promise((resolve, reject) => {
       queue.push({ id, makePromise, resolve, reject, tag });
       tryStart();
