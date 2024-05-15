rvedotrc/dropbox-hacking

commit 58a59338e6885ea3ad998ff637d59577a553b606

Dynamically calculate build order

dropbox-hacking-photo-manager-server/src/server/contextBuilder.ts
@@ -1,3 +1,4 @@
+import { Dropbox } from "dropbox";
 import { ExifDB } from "dropbox-hacking-exif-db";
 import * as LsCache from "dropbox-hacking-ls-cache";
 import { getDropboxClient } from "dropbox-hacking-util";
@@ -32,24 +33,25 @@ export default (args: {
   return {
     port: args.port,
     baseUrlWithoutSlash: args.baseUrlWithoutSlash,
-    get dropboxClient() {
+    get dropboxClient(): Promise<Dropbox> {
       return getDropboxClient();
     },
-    get lsState() {
-      if (!lsCacheState) {
+    get lsState(): Promise<LsCache.State> {
+      if (lsCacheState !== undefined) {
         const lsCache = new LsCache.StateDir(lsCacheDir);
         lsCacheState = lsCache.load().then(() => lsCache.getState());
       }
+      if (lsCacheState === undefined) throw "";
       return lsCacheState;
     },
-    get exifDbAll() {
+    get exifDbAll(): ReturnType<ExifDB["readAll"]> {
       if (!exifAll) {
         const exifDb = new ExifDB(exifDbDir);
         exifAll = exifDb.readAll();
       }
       return exifAll;
     },
-    get dayDb() {
+    get dayDb(): Promise<DayDb> {
       return Promise.resolve(new DayDb(dayDbDir));
     },
   };

build.sh
@@ -2,26 +2,37 @@
 
 set -ex
 
-# Because the build-order doesn't work yet
-
-yarn workspace dropbox-hacking-util build
-
-yarn workspace dropbox-hacking-downloader build &
-yarn workspace dropbox-hacking-lister build &
-yarn workspace dropbox-hacking-ls-cache build &
-yarn workspace dropbox-hacking-mover build &
-yarn workspace dropbox-hacking-uploader build &
+for w in $( yarn --silent workspaces info | jq -r 'keys[]' ) ; do
+  yarn workspace $w run prettier &
+done
 wait
 
-yarn workspace dropbox-hacking-exif-db build &
-yarn workspace dropbox-hacking-mediainfo-db build &
-yarn workspace dropbox-hacking-sync build &
+for w in $( yarn --silent workspaces info | jq -r 'keys[]' ) ; do
+  yarn workspace $w run lint &
+done
 wait
 
-yarn workspace dropbox-hacking-cli build &
-yarn workspace dropbox-hacking-photo-manager-shared build &
-wait
+ruby -rjson <<'RUBY' | $SHELL
+data = JSON.parse(`jq '[input_filename, [(.references // [])[].path]]' dropbox-hacking-*/tsconfig.json | jq --slurp -c`)
+refs = data.to_h
+refs.transform_keys! { |k| k.sub("/tsconfig.json", "") }
+refs.transform_values! { |l| l.map { |k| k.sub("../", "") } }
+satisfied = []
+unsatisfied = refs.keys
 
-yarn workspace dropbox-hacking-photo-manager-server build &
-yarn workspace dropbox-hacking-photo-manager-client build &
-wait
+while !unsatisfied.empty?
+  can_satisfy = unsatisfied.select { |m| (refs[m] - satisfied).empty? }
+  raise "Stuck!" if can_satisfy.empty?
+
+  satisfied += can_satisfy
+  unsatisfied -= can_satisfy
+
+  can_satisfy.each do |m|
+    puts "yarn workspace #{m} compile &"
+  end
+  puts "wait"
+  puts
+end
+RUBY
+
+exit

dropbox-hacking-cli/src/operations/syncDownload.ts
@@ -20,12 +20,12 @@ const makeContentHashFetcher = () => {
   const cache = new Map<string, Promise<string>>();
 
   return (localPath: string): Promise<string> => {
-    let promise = cache.get(localPath);
-    if (promise) return promise;
+    const promise1 = cache.get(localPath);
+    if (promise1 !== undefined) return promise1;
 
-    promise = makeContentHash(fs.createReadStream(localPath));
-    cache.set(localPath, promise);
-    return promise;
+    const promise2 = makeContentHash(fs.createReadStream(localPath));
+    cache.set(localPath, promise2);
+    return promise2;
   };
 };
 

dropbox-hacking-downloader/src/index.ts
@@ -5,56 +5,90 @@ import * as fs from "fs";
 import * as http from "http";
 import * as https from "https";
 
-const defaultLimiter = makePromiseLimiter(2, "download-limiter");
+const defaultLimiter = makePromiseLimiter<void>(2, "download-limiter");
+const defaultMode = 0o644;
+const defaultInactivityTimeout = 300 * 1000;
 
+const downloadToFilehandle = (args: {
+  dbx: Dropbox;
+  remote: files.FileMetadata;
+  fh: fs.promises.FileHandle;
+  inactivityTimeout?: number;
+  limiter?: typeof defaultLimiter;
+}): Promise<void> => {
+  const w = args.fh.createWriteStream();
+
+  let timer: NodeJS.Timeout | undefined;
+
+  return args.dbx
+    .filesGetTemporaryLink({ path: `rev:${args.remote.rev}` })
+    .then((r) => r.result.link)
+    .then((uri) =>
+      new Promise<void>((resolve, reject) => {
+        const req = https
+          .get(uri, {}, (res: http.IncomingMessage) => {
+            res.pipe(w);
+            res.on("error", reject);
+            res.on("end", resolve);
+            w.on("error", reject);
+          })
+          .on("error", reject)
+          .on("abort", reject)
+          .on("timeout", reject);
+        timer = setTimeout(() => {
+          req.destroy(new Error("Timeout hit, aborting"));
+        }, args.inactivityTimeout);
+      }).finally(() => timer && clearTimeout(timer)),
+    )
+    .then(
+      () =>
+        new Promise((resolve, reject) =>
+          w.close((err) => (err ? reject(err) : resolve(undefined))),
+        ),
+      (err) => {
+        w.close(() => {
+          throw err;
+        });
+      },
+    );
+};
+
+// Download 'remote' to 'local' using 'dbx'. The download happens to a
+// temporary file with mode 0o000, and is then flushed, has its mtime
+// set, mode set to 0o644 (or as specified) before being renamed into
+// place. The whole thing is done behind a limiter, and with an abort-
+// -on-inactivity timeout.
 export const downloader = (args: {
   dbx: Dropbox;
   local: string;
   remote: files.FileMetadata;
+  mode?: number;
+  inactivityTimeout?: number;
+  limiter?: typeof defaultLimiter;
 }): Promise<void> => {
-  const { dbx, local, remote } = args;
+  const { local, remote } = args;
 
   const id = randomUUID().toString();
   const tmpLocal = `${local}.tmp.dbxsync.${id}`;
   const mtime = parseTime(remote.client_modified);
+  const mode = args.mode === undefined ? defaultMode : args.mode;
+  const limiter = args.limiter || defaultLimiter;
+  const inactivityTimeout = args.inactivityTimeout || defaultInactivityTimeout;
 
-  // const log =
-  //   <T>(message: string) =>
-  //   (result: T): T => {
-  //     console.debug(`download ${local}: ${message}`);
-  //     return result;
-  //   };
-
-  return defaultLimiter
-    .submit(() => {
-      const w = fs.createWriteStream(tmpLocal, { mode: 0o644, flags: "wx" });
-
-      let timer: NodeJS.Timeout | undefined;
-
-      return dbx
-        .filesGetTemporaryLink({ path: `rev:${remote.rev}` })
-        .then((r) => r.result.link)
-        .then((uri) =>
-          new Promise<void>((resolve, reject) => {
-            const req = https
-              .get(uri, {}, (res: http.IncomingMessage) => {
-                res.pipe(w);
-                res.on("error", reject);
-                res.on("end", resolve);
-                w.on("error", reject);
-              })
-              .on("error", reject)
-              .on("abort", reject)
-              .on("timeout", reject);
-            timer = setTimeout(() => {
-              req.destroy(new Error("Timeout hit, aborting"));
-            }, 300 * 1000);
-          }).finally(() => timer && clearTimeout(timer)),
+  return limiter.submit(
+    () =>
+      fs.promises
+        .open(tmpLocal, "wx", 0o000)
+        .then((fh) =>
+          downloadToFilehandle({ ...args, inactivityTimeout, fh }).finally(() =>
+            fh.close(),
+          ),
         )
         .then(() => fs.promises.utimes(tmpLocal, mtime, mtime))
+        .then(() => fs.promises.chmod(tmpLocal, mode))
         .then(() => fs.promises.rename(tmpLocal, local))
-        .finally(() => w.close())
-        .finally(() => fs.unlink(tmpLocal, () => {}));
-    }, `download ${remote.path_display} => ${local}`)
-    .then();
+        .finally(() => fs.unlink(tmpLocal, () => {}))
+        .then((): void => {}),
+    `download ${remote.path_display} => ${local}`,
+  );
 };

dropbox-hacking-util/src/auth.ts
@@ -1,6 +1,6 @@
 import * as child_process from "child_process";
 import { Dropbox, DropboxAuth } from "dropbox";
-import * as express from "express";
+import express from "express";
 import * as fs from "fs";
 
 import { writeStderr } from "./logging";

commit 2d9739a232a1cbf6f2a114852d4d89da6a36d0e6

Fix bug introduced in 58a59338e6885ea3ad998ff637d59577a553b606

dropbox-hacking-photo-manager-client/src/listOfDaysNoPics.tsx
@@ -1,0 +1,279 @@
+import * as React from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+  DayMetadata,
+  Payload,
+  ThumbnailsByRevResponse,
+} from "dropbox-hacking-photo-manager-shared";
+
+import SamePageLink from "./samePageLink";
+import { useDaysMetadata } from "./daysMetadataContext";
+import { useCountsByDate } from "./countsByDateContext";
+
+const ListOfDays = ({ setState }: { setState: (payload: Payload) => void }) => {
+  const countsByDate = useCountsByDate();
+  const dayMetadata = useDaysMetadata();
+
+  const [revToThumbnail, setRevToThumbnail] = useState(
+    new Map<string, string | undefined>(),
+  );
+  const [visibleDays, setVisibleDays] = useState<[number, number]>();
+
+  useEffect(() => {
+    document.title = "DPM - Days";
+  });
+
+  // Discard any unwanted thumbnails
+  useEffect(() => {
+    if (countsByDate) {
+      const wantedRevs = new Set(
+        [...countsByDate.values()]
+          .flatMap((e) => e.samplePhotos)
+          .map((p) => p.rev),
+      );
+
+      let changed = false;
+      for (const rev of revToThumbnail.keys()) {
+        if (!wantedRevs.has(rev)) {
+          revToThumbnail.delete(rev);
+          changed = true;
+        }
+      }
+
+      if (changed) setRevToThumbnail(new Map(revToThumbnail));
+    }
+  }, [countsByDate, revToThumbnail]);
+
+  // Load any missing thumbnails (visible days only)
+  useEffect(() => {
+    console.log("useEffect img load", countsByDate !== undefined, visibleDays);
+
+    if (countsByDate === undefined) return;
+    if (!visibleDays) return;
+
+    const wantedRevs = new Set(
+      countsByDate
+        .slice(visibleDays[0], visibleDays[1] + 1)
+        .flatMap((e) => e.samplePhotos)
+        .map((p) => p.rev),
+    );
+
+    const needToRequest: string[] = [];
+
+    for (const rev of wantedRevs) {
+      if (!revToThumbnail.has(rev)) {
+        needToRequest.push(rev);
+        // Use undefined to record the fact that we have made the request
+        revToThumbnail.set(rev, undefined);
+      }
+    }
+
+    needToRequest.sort();
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const slice = needToRequest.splice(0, 25);
+      if (slice.length === 0) break;
+
+      console.log(`Requesting ${slice.join(",")}`);
+
+      fetch(`/api/thumbnail/128/revs/${slice.join(",")}`)
+        .then((res) => res.json() as Promise<ThumbnailsByRevResponse>)
+        .then((data) => {
+          console.log(`Got response for ${slice.join(",")}`, data);
+
+          for (const r of data.thumbnails_by_rev) {
+            console.log(`got thumbnail for ${r.rev}`);
+            revToThumbnail.set(r.rev, r.thumbnail);
+          }
+
+          setRevToThumbnail(new Map(revToThumbnail));
+          // console.log("fetch complete");
+        });
+    }
+  }, [countsByDate, revToThumbnail, visibleDays]);
+
+  const olRef = useRef<HTMLOListElement>(null);
+  console.log(
+    "render",
+    olRef.current,
+    visibleDays,
+    countsByDate ? countsByDate.length : null,
+  );
+  // console.log("render", olRef.current, visibleDays, (visibleDays && countsByDate) ? [countsByDate[visibleDays[0]].date, countsByDate[visibleDays[1]].date] : null);
+
+  useEffect(() => {
+    console.log("use effect scroll", olRef.current);
+    const ol = olRef.current;
+    if (!ol) return;
+
+    const onScrollStopped = () => {
+      console.log("scroll has stopped");
+      console.log(
+        `window sY=${window.scrollY} sT=${window.screenTop} iH=${window.innerHeight} oH=${window.outerHeight}`,
+      );
+
+      const body = document.body;
+      console.log(
+        `body oH/T=${body.offsetHeight}/${body.offsetTop} sH/T=${body.scrollHeight}/${body.scrollTop} cH/T=${body.clientHeight}/${body.clientTop}`,
+      );
+
+      console.log(
+        `ol oH/T=${ol.offsetHeight}/${ol.offsetTop} sH/T=${ol.scrollHeight}/${ol.scrollTop} cH/T=${ol.clientHeight}/${ol.clientTop}`,
+      );
+
+      const items = [...ol.childNodes] as HTMLLIElement[];
+
+      // Each item is either above the viewport, or below it, or overlapping.
+      // Above, overlapping, below, in that order.
+      // We need to find the ones which are overlapping.
+
+      const queryItem = (i: number) => {
+        // if (i > 20) return +1;
+
+        const element = items[i];
+        if (!element) throw "No item";
+
+        const box = element.getBoundingClientRect();
+
+        const answer =
+          box.bottom < 0 ? -1 : box.top > window.innerHeight ? +1 : 0;
+        // console.log(`i=${i} ${answer} date=${countsByDate?.[i].date} box t/b/y/h=${box.top}/${box.bottom}/${box.y}/${box.height}`);
+        return answer;
+      };
+
+      console.log(
+        `window screenY=${window.screenY} screenTop=${window.screenTop} scrollY=${window.scrollY} innerHeight=${window.innerHeight} outerHeight=${window.outerHeight}`,
+      );
+      const v = (window as any).visualViewport;
+      console.log(`v h=${v.height} oT=${v.offsetTop} pageTop=${v.pageTop}`);
+      const q = items.map((_ele, i) => queryItem(i));
+      console.log(`q = ${q.join(" ")}`);
+      const min = q.indexOf(0);
+      const max = q.lastIndexOf(0);
+      setVisibleDays([min, max]);
+    };
+
+    let timer = window.setTimeout(onScrollStopped, 500);
+
+    const listener = (_event: Event) => {
+      if (timer) window.clearTimeout(timer);
+      timer = window.setTimeout(onScrollStopped, 500);
+    };
+
+    window.addEventListener("scroll", listener);
+    window.addEventListener("resize", listener);
+    ol.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("scroll", listener);
+      window.removeEventListener("resize", listener);
+      ol.removeEventListener("resize", listener);
+      if (timer) window.clearTimeout(timer);
+    };
+  }, [countsByDate, dayMetadata]);
+
+  if (!countsByDate || !dayMetadata) {
+    return <div>Loading...</div>;
+  }
+
+  const keyedMetadata = new Map<string, DayMetadata>(
+    dayMetadata.map((m) => [m.date, m]),
+  );
+  const days = countsByDate.map((e) => ({
+    ...e,
+    description: (keyedMetadata.get(e.date) || { description: "" }).description,
+  }));
+
+  let daysWith = 0;
+  let daysWithout = 0;
+  let photosWith = 0;
+  let photosWithout = 0;
+
+  for (const day of days) {
+    if (day.description !== "") {
+      ++daysWith;
+      photosWith += day.count;
+    } else {
+      ++daysWithout;
+      photosWithout += day.count;
+    }
+  }
+
+  return (
+    <div>
+      <h1>List of Days</h1>
+
+      <p>
+        <SamePageLink
+          href={"/calendar"}
+          state={{ route: "calendar" }}
+          setState={setState}
+        >
+          Calendar
+        </SamePageLink>
+      </p>
+
+      <p>
+        Days with; without; percent-with; count = {daysWith}; {daysWithout};{" "}
+        {Math.round(((100.0 * daysWith) / (daysWith + daysWithout)) * 100.0) /
+          100.0}
+        ; {daysWith + daysWithout}
+      </p>
+
+      <p>
+        Photos with; without; percent-with; count = {photosWith};{" "}
+        {photosWithout};{" "}
+        {Math.round(
+          ((100.0 * photosWith) / (photosWith + photosWithout)) * 100.0,
+        ) / 100.0}
+        ; {photosWith + photosWithout}
+      </p>
+
+      <ol ref={olRef} className={"listOfDays"}>
+        {days.map((day) => (
+          <li key={day.date}>
+            <SamePageLink
+              state={{ route: "day", date: day.date }}
+              setState={setState}
+              href={`/day/${day.date}`}
+            >
+              <span className={"date"}>{day.date}</span>
+              <span className={"count"}>{day.count}</span>
+              <span className={"countWithGps"}>{day.countWithGps}</span>
+              <span className={"description"}>
+                {day.description === undefined ? "-" : day.description}
+              </span>
+              <span className={"samples"}>
+                {day.samplePhotos.map((photo) => {
+                  const thumbnail = revToThumbnail.get(photo.rev);
+                  return (
+                    <span
+                      key={photo.id}
+                      style={{
+                        display: "inline-block",
+                        height: "128px",
+                        width: "128px",
+                      }}
+                    >
+                      <img
+                        src={
+                          thumbnail
+                            ? `data:image/jpeg;base64,${thumbnail}`
+                            : `/placeholder.png`
+                        }
+                        alt={"thumbnail"}
+                      />
+                    </span>
+                  );
+                })}
+              </span>
+            </SamePageLink>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default ListOfDays;

dropbox-hacking-photo-manager-server/src/server/contextBuilder.ts
@@ -37,15 +37,14 @@ export default (args: {
       return getDropboxClient();
     },
     get lsState(): Promise<LsCache.State> {
-      if (lsCacheState !== undefined) {
+      if (lsCacheState === undefined) {
         const lsCache = new LsCache.StateDir(lsCacheDir);
         lsCacheState = lsCache.load().then(() => lsCache.getState());
       }
-      if (lsCacheState === undefined) throw "";
       return lsCacheState;
     },
     get exifDbAll(): ReturnType<ExifDB["readAll"]> {
-      if (!exifAll) {
+      if (exifAll === undefined) {
         const exifDb = new ExifDB(exifDbDir);
         exifAll = exifDb.readAll();
       }