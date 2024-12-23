6f84849a5d4519e360768c79a9bae4ddf853ae30
Rachel Evans <rachel@rachelevans.org>
2024-12-23 18:26:21 +0100

    Avoid jumping as images load

--- dropbox-hacking-photo-manager-client/src/days/samplePhoto.tsx
+++ dropbox-hacking-photo-manager-client/src/days/samplePhoto.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Photo } from "dropbox-hacking-photo-manager-shared";
 import { useThumbnail } from "../context/thumbnails/useThumbnail";
 import { useThumbnailLoader } from "../context/thumbnails";
+import logRender from "../logRender";
 
 const samplePhoto = ({
   photo,
@@ -14,14 +15,7 @@ const samplePhoto = ({
   const thumbnail = useThumbnail(photo.rev, loader)(visible);
 
   return (
-    <span
-      key={photo.id}
-      style={{
-        display: "inline-block",
-        height: "128px",
-        width: "128px",
-      }}
-    >
+    <span key={photo.id} className="sample">
       <img
         src={
           thumbnail ? `data:image/jpeg;base64,${thumbnail}` : `/placeholder.png`
@@ -32,4 +26,4 @@ const samplePhoto = ({
   );
 };
 
-export default samplePhoto;
+export default logRender(samplePhoto);
