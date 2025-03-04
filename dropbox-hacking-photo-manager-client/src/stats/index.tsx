6dfbffd7a5450b052f5ff5d32a1acea7fc3cbe9e
Rachel Evans <rachel@rachelevans.org>
2025-03-04 08:12:15 +0100

    Two new experimental join feeds

--- dropbox-hacking-photo-manager-client/src/stats/index.tsx
+++ dropbox-hacking-photo-manager-client/src/stats/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { map, Observable } from "rxjs";
+import { useEffect, useMemo, useState } from "react";
+import { combineLatest, map, Observable } from "rxjs";
 
 import logRender from "../logRender";
 import { useRxFeedsViaMultiplexer } from "../context/rx/rxRecordFeedContext";
 import type { ImageAndMaybeDelta } from "../context/rx/rxFeedClient";
 import Navigate from "../days/navigate";
+import type { ExifFromHash } from "dropbox-hacking-exif-db";
+import type {
+  NamedFile,
+  PhotoDbEntry,
+} from "dropbox-hacking-photo-manager-shared";
+import { useLatestValue } from "../context/rx/useLatestValue";
 
 const Stats = () => {
   const feeds = useRxFeedsViaMultiplexer();
@@ -42,24 +48,78 @@ const Stats = () => {
   addEffect("files", setFilesCount);
   addEffect("photos", setPhotosCount);
 
+  const filesAndExifAndPhotoDb = useMemo(
+    () =>
+      !feeds
+        ? undefined
+        : // Naive full join
+          combineLatest([feeds.files, feeds.exif, feeds.photos]).pipe(
+            map(([f, e, p]) => {
+              const out: Record<
+                string,
+                {
+                  namedFile: NamedFile;
+                  exif: ExifFromHash | undefined;
+                  photoDbEntry: PhotoDbEntry | undefined;
+                }
+              > = {};
+
+              for (const [id, namedFile] of Object.entries(f.image)) {
+                const exif: ExifFromHash | undefined =
+                  e.image[namedFile.content_hash];
+                const photoDbEntry: PhotoDbEntry | undefined = p.image[id];
+                out[id] = { namedFile, exif, photoDbEntry };
+              }
+              return out;
+            }),
+          ),
+    [feeds],
+  );
+
+  const photosByDate = useMemo(
+    () =>
+      filesAndExifAndPhotoDb?.pipe(map((t) => Object.values(t))).pipe(
+        map((photos) => {
+          const out: Record<string, typeof photos> = {};
+
+          for (const photo of photos) {
+            const date = photo.namedFile.client_modified.substring(0, 10);
+            const list = out[date];
+            if (list) list.push(photo);
+            else out[date] = [photo];
+          }
+
+          return out;
+        }),
+      ),
+    [filesAndExifAndPhotoDb],
+  );
+
+  const fepLatest = useLatestValue(filesAndExifAndPhotoDb);
+
+  const fepExtract = useMemo(
+    () =>
+      !fepLatest
+        ? null
+        : Object.entries(fepLatest)
+            .toSorted((a, b) => a[0].localeCompare(b[0]))
+            .slice(0, 3),
+    [fepLatest],
+  );
+
+  const pbdLatest = useLatestValue(photosByDate);
+
+  const pbdExtract = useMemo(
+    () =>
+      pbdLatest
+        ? Object.entries(pbdLatest)
+            .toSorted((a, b) => b[0].localeCompare(a[0]))
+            .slice(0, 3)
+        : null,
+    [pbdLatest],
+  );
+
   if (!feeds) return "waiting for multiplexer...";
-
-  //   const [filesWithExifCount, setFilesWithExifCount] = useState<number>();
-  //   const [fileExifCountsByMonth, setFileExifCountsByMonth] =
-  //     useState<Record<string, number>>();
-
-  //     const subFilesWithExif = combineLatest([
-  //       rxFeeds.file,
-  //       rxFeeds.exif,
-  //     ]).subscribe(([files, exifs]) => {
-  //       const out: Record<string, { file: NamedFile; exif?: ExifFromHash }> = {};
-
-  //       for (const f of Object.values(files)) {
-  //         out[f.path_lower] = { file: f, exif: exifs[f.content_hash] };
-  //       }
-
-  //       setFilesWithExifCount(Object.keys(out).length);
-  //     });
 
   return (
     <>
@@ -79,11 +139,13 @@ const Stats = () => {
       <h2>Photos</h2>
       <p>Count: {photosCount ?? "?"}</p>
 
-      {/* <h2>Files & Exif</h2>
-        <p>Count: {filesWithExifCount ?? "?"}</p>
+      <h2>Here be dragons!</h2>
 
-        <h2>File-Exif counts by month</h2>
-        {JSON.stringify(fileExifCountsByMonth ?? null)} */}
+      <h3>File, Exif, PhotoDB</h3>
+      <pre>{JSON.stringify(fepExtract, null, 2)}</pre>
+
+      <h3>Photos by date</h3>
+      <pre>{JSON.stringify(pbdExtract, null, 2)}</pre>
     </>
   );
 };
