22747ee102bbf6176953a40839b0613252e35400
Rachel Evans <rachel@rachelevans.org>
2025-07-12 12:05:51 +0200

    chore: disable the unhelpful 'no-unsafe-' lint rules

--- dropbox-hacking-photo-manager-client/eslint.config.mjs
+++ dropbox-hacking-photo-manager-client/eslint.config.mjs
@@ -39,18 +39,17 @@ export default tseslint.config(
       ],
       "@typescript-eslint/require-await": "off",
 
-      // *So* many false positives...
-      // and also wildly unhelpful error messages.
+      // These rules mostly seem good at hiding the real cause of
+      // the error in a much less comprehensible error message.
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-call": "off",
-      // -declaration-merging
-      // -enum-comparison
-      // -function-type
+      "@typescript-eslint/no-unsafe-comparison": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
-      // -return
-      // -type-assertion
-      // -unary-minus
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-unary-minus": "off",
     },
   },
   {
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -59,6 +59,18 @@ export default [
           caughtErrorsIgnorePattern: "^_",
         },
       ],
+
+      // These rules mostly seem good at hiding the real cause of
+      // the error in a much less comprehensible error message.
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-comparison": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-unary-minus": "off",
     },
   },
 ];
